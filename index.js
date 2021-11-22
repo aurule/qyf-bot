d6950544ec8a3e56f3686ec8dd5342ee4f30589c
Peter Andrews <pmandrews@gmail.com>
2021-11-22 16:21:36 -0500

    Create basic client

---
+++ index.js
@@ -0,0 +1,17 @@
+// Load envvars
+require('dotenv').config()
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const token = process.env.BOT_TOKEN;
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
