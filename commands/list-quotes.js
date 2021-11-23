ca36399e26b30fb5500d8b5dfa86f12f0bd1a169
Peter Andrews <pmandrews@gmail.com>
2021-11-23 16:23:50 -0500

    Add scaffolding for rand quote command

---
+++ commands/list-quotes.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('list-quotes')
+        .setDescription('Show the most recent quotes from the current game')
+        .addUserOption(option =>
+            option.setName('speaker')
+            .setDescription('By a user'))
+        .addStringOption(option =>
+            option.setName('alias')
+            .setDescription('By a name'))
+        .addStringOption(option =>
+            option.setName('text')
+            .setDescription('Containing some text'))
+        .addStringOption(option =>
+            option.setName('game')
+            .setDescription('From a game'))
+        .addIntegerOption(option =>
+            option.setName('amount')
+            .setDescription('Number of quotes to show (1-10)')),
+    async execute(interaction) {
+        const speaker = interaction.options.getUser('speaker');
+        const alias = interaction.options.getString('alias');
+        const text = interaction.options.getString('text');
+        const game = interaction.options.getString('game');
+        const arg_amount = interaction.options.getInteger('amount');
+
+        const amount = arg_amount ? arg_amount : 5;
+
+        await interaction.reply(`Showing ${amount} quotes`);
+    },
+};
