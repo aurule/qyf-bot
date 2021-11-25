5752bd9b9650a238f3522e97b660014e59025c12
Peter Andrews <pmandrews@gmail.com>
2021-11-25 12:39:37 -0500

    Create a default game for newly joined guilds

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,11 +1,16 @@
-const { Guilds } = require("../models");
+const { Guilds, Games } = require("../models");
 
 module.exports = {
-    name: "guildCreate",
-    async execute(guild) {
-        await Guilds.upsert({
-            name: guild.name,
-            snowflake: guild.id,
-        });
-    },
+  name: "guildCreate",
+  async execute(guild) {
+    const [guild_record, _isNew] = await Guilds.upsert({
+      name: guild.name,
+      snowflake: guild.id,
+    });
+
+    await Games.upsert({
+      name: "No Game",
+      guildId: guild_record.id,
+    });
+  },
 };
