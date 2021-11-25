cc0f65f06c204e821a5d61c135728158d84da2e0
Peter Andrews <pmandrews@gmail.com>
2021-11-25 14:34:13 -0500

    Use findOrCreate instead of Upsert Upsert has been returning invalid record IDs in local testing, so it has to go.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,15 +2,27 @@ const { Guilds, Games, DefaultGames } = require("../models");
 
 module.exports = {
   name: "guildCreate",
-  async execute(guild) {
-    const [guild_record, _isNew] = await Guilds.upsert({
-      name: guild.name,
-      snowflake: guild.id,
+  async execute(discord_guild) {
+    const [guild, _isNewGuild] = await Guilds.findOrCreate({
+      where: {
+        snowflake: discord_guild.id
+      },
+      defaults: {
+        name: discord_guild.name,
+        snowflake: discord_guild.id,
+      }
     });
 
-    await Games.upsert({
-      name: "No Game",
-      guildId: guild_record.id,
+    const [game, _isNewGame] = await Games.findOrCreate({
+      where: {
+        name: "No Game",
+        guildId: guild.id,
+      },
+      defaults: {
+        name: "No Game",
+        guildId: guild.id,
+      }
+    });
 
     DefaultGames.findOrCreate({
       where: {snowflake: guild.snowflake},
