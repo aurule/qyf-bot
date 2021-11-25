e29e9b94dd63d9863842f2e0c982f986e1764bed
Peter Andrews <pmandrews@gmail.com>
2021-11-25 15:39:13 -0500

    Fix formatting

--- commands/add-game.js
+++ commands/add-game.js
@@ -1,16 +1,18 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('add-game')
-        .setDescription('Add a game to this server')
-        .addStringOption(option =>
-            option.setName('name')
-            .setDescription('The name of the game')
-            .setRequired(true)),
-    async execute(interaction) {
-        const game_name = interaction.options.getString('name');
+  data: new SlashCommandBuilder()
+    .setName("add-game")
+    .setDescription("Add a game to this server")
+    .addStringOption((option) =>
+      option
+        .setName("name")
+        .setDescription("The name of the game")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const game_name = interaction.options.getString("name");
 
-        await interaction.reply(`Added game "${game_name}"`);
-    },
+    await interaction.reply(`Added game "${game_name}"`);
+  },
 };
