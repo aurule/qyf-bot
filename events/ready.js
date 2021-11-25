d49729d337c4d806852ea82099a4546f897fc4e6
Peter Andrews <pmandrews@gmail.com>
2021-11-25 12:38:26 -0500

    Formatting fix

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
-const logger = require('pino')()
+const logger = require("pino")();
 
 module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        logger.info(`Ready! Logged in as ${client.user.tag}`);
-    },
+  name: "ready",
+  once: true,
+  execute(client) {
+    logger.info(`Ready! Logged in as ${client.user.tag}`);
+  },
 };
