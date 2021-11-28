9f9cf121f9a637315878a762003fbb474e00db8e
Peter Andrews <pmandrews@gmail.com>
2021-11-28 16:10:42 -0500

    Fix formatting

--- commands/set-default-game.js
+++ commands/set-default-game.js
@@ -5,55 +5,55 @@ const { Guilds, Games, DefaultGames } = require("../models");
 const { transform } = require("../transformers/gameSelectTransformer");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("set-default-game")
-        .setDescription("Set the default game for this channel")
-        .addChannelOption((option) =>
-            option.setName("channel").setDescription("The target channel")
-        )
-        .addBooleanOption((option) =>
-            option
-                .setName("server")
-                .setDescription("Apply default to the whole server")
-        ),
-    async execute(interaction) {
-        const current_channel = interaction.channel;
-        const channel_option = interaction.options.getChannel("channel");
-        const target_channel = channel_option
-            ? channel_option
-            : current_channel;
-        const server_wide = interaction.options.getBoolean("server");
+  data: new SlashCommandBuilder()
+    .setName("set-default-game")
+    .setDescription("Set the default game for this channel")
+    .addChannelOption((option) =>
+      option.setName("channel").setDescription("The target channel")
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("server")
+        .setDescription("Apply default to the whole server")
+    ),
+  async execute(interaction) {
+    const current_channel = interaction.channel;
+    const channel_option = interaction.options.getChannel("channel");
+    const target_channel = channel_option ? channel_option : current_channel;
+    const server_wide = interaction.options.getBoolean("server");
 
-        const command_options = this.followupOptions(target_channel, server_wide);
-        keyv.set(interaction.id, command_options);
+    const command_options = this.followupOptions(target_channel, server_wide);
+    keyv.set(interaction.id, command_options);
 
-        const guild = await Guilds.findByInteraction(interaction, {include: Games});
+    const guild = await Guilds.findByInteraction(interaction, {
+      include: Games,
+    });
 
-        const gameSelectRow = new MessageActionRow().addComponents(
-            new MessageSelectMenu()
-                .setCustomId("defaultGameSelect")
-                .setPlaceholder("Pick a game")
-                .addOptions(transform(guild.Games))
-        );
+    const gameSelectRow = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId("defaultGameSelect")
+        .setPlaceholder("Pick a game")
+        .addOptions(transform(guild.Games))
+    );
 
-        return interaction.reply({
-            content: `Which game do you want to set as the default for ${command_options.scope_text}?`,
-            components: [gameSelectRow],
-            ephemeral: true,
-        });
-    },
-    followupOptions(target_channel, server_wide) {
-        if (server_wide) {
-            return {
-                scope_text: "the server",
-                target_type: DefaultGames.TYPE_GUILD,
-                target_snowflake: target_channel.guild.id.toString(),
-            }
-        }
-        return {
-            scope_text: target_channel.name,
-            target_type: DefaultGames.TYPE_CHANNEL,
-            target_snowflake: target_channel.id.toString(),
-        }
+    return interaction.reply({
+      content: `Which game do you want to set as the default for ${command_options.scope_text}?`,
+      components: [gameSelectRow],
+      ephemeral: true,
+    });
+  },
+  followupOptions(target_channel, server_wide) {
+    if (server_wide) {
+      return {
+        scope_text: "the server",
+        target_type: DefaultGames.TYPE_GUILD,
+        target_snowflake: target_channel.guild.id.toString(),
+      };
     }
+    return {
+      scope_text: target_channel.name,
+      target_type: DefaultGames.TYPE_CHANNEL,
+      target_snowflake: target_channel.id.toString(),
+    };
+  },
 };
--- commands/set-default-game.test.js
+++ commands/set-default-game.test.js
@@ -19,8 +19,8 @@ beforeEach(async () => {
     });
     interaction = new Interaction(guild.snowflake);
     interaction.channel.id = simpleflake();
-    interaction.channel.guild = {id: guild.snowflake};
-    interaction.channel.name = 'test channel';
+    interaction.channel.guild = { id: guild.snowflake };
+    interaction.channel.name = "test channel";
   } catch (err) {
     console.log(err);
   }
@@ -28,7 +28,7 @@ beforeEach(async () => {
 
 afterEach(async () => {
   try {
-    await Games.destroy({where: {guildId: guild.id}})
+    await Games.destroy({ where: { guildId: guild.id } });
     await guild.destroy();
   } catch (err) {
     console.log(err);
@@ -36,32 +36,34 @@ afterEach(async () => {
 });
 
 describe("followupOptions", () => {
-  describe('when server wide flag is true', () => {
+  describe("when server wide flag is true", () => {
     beforeEach(() => {
       interaction.command_options.server = true;
-    })
+    });
 
     it("sets scope text to the server", async () => {
       await set_default_game_command.execute(interaction);
       const stored_options = await keyv.get(interaction.id);
 
       expect(stored_options.scope_text).toBe("the server");
-    })
+    });
 
     it("sets the target type to guild", async () => {
       await set_default_game_command.execute(interaction);
       const stored_options = await keyv.get(interaction.id);
 
       expect(stored_options.target_type).toBe(DefaultGames.TYPE_GUILD);
-    })
+    });
 
     it("stores the server snowflake", async () => {
       await set_default_game_command.execute(interaction);
       const stored_options = await keyv.get(interaction.id);
 
-      expect(stored_options.target_snowflake).toEqual(guild.snowflake.toString());
-    })
-  })
+      expect(stored_options.target_snowflake).toEqual(
+        guild.snowflake.toString()
+      );
+    });
+  });
 
   describe("when server wide flag is false", () => {
     it("sets scope text to chosen channel reference", async () => {
@@ -69,39 +71,41 @@ describe("followupOptions", () => {
       const stored_options = await keyv.get(interaction.id);
 
       expect(stored_options.scope_text).toEqual(interaction.channel.name);
-    })
+    });
 
     it("sets the target type to channel", async () => {
       await set_default_game_command.execute(interaction);
       const stored_options = await keyv.get(interaction.id);
 
       expect(stored_options.target_type).toBe(DefaultGames.TYPE_CHANNEL);
-    })
+    });
 
     it("stores the channel snowflake", async () => {
       await set_default_game_command.execute(interaction);
       const stored_options = await keyv.get(interaction.id);
 
-      expect(stored_options.target_snowflake).toEqual(interaction.channel.id.toString());
-    })
-  })
+      expect(stored_options.target_snowflake).toEqual(
+        interaction.channel.id.toString()
+      );
+    });
+  });
 });
 
 describe("reply", () => {
-  it('includes an action row', async () => {
+  it("includes an action row", async () => {
     const reply = await set_default_game_command.execute(interaction);
 
-    expect(reply.components).not.toBeFalsy()
-  })
+    expect(reply.components).not.toBeFalsy();
+  });
 
   it("shows a select for the guild's games", async () => {
     const game = await Games.create({
-      name: 'test game',
-      guildId: guild.id
-    })
-    const transformSpy = jest.spyOn(GameSelectTransformer, 'transform');
+      name: "test game",
+      guildId: guild.id,
+    });
+    const transformSpy = jest.spyOn(GameSelectTransformer, "transform");
 
-    const reply = await set_default_game_command.execute(interaction)
+    const reply = await set_default_game_command.execute(interaction);
     const selectOptions = reply.components[0].components[0].options;
 
     expect(selectOptions[0].label).toEqual(game.name);
