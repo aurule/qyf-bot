4339f718ef93a3a369f9e0de0d543fac9d3811a8
Peter Andrews <pmandrews@gmail.com>
2021-12-02 10:44:39 -0500

    Document select transformer

--- transformers/game-select-transformer.js
+++ transformers/game-select-transformer.js
@@ -1,6 +1,12 @@
 "use strict"
 
 module.exports = {
+  /**
+   * Transforms a collection of games into an array of label/value objects
+   * suitable for use in a Discord select menu interaction reply.
+   * @param  {Collection} games The games to transform
+   * @return {Array[Obj]}       Array of select menu option objects
+   */
   transform: (games) => {
     return games.map((game) => {
       return {
