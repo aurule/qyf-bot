e9b0d617ec53f2e7c639871d18ba6d05f822f61e
Peter Andrews <pmandrews@gmail.com>
2021-12-02 11:47:43 -0500

    Add log pretty printer for development

--- util/logger.js
+++ util/logger.js
@@ -1,5 +1,16 @@
 const Pino = require("pino")
+const pretty = require('pino-pretty')
+require("dotenv").config()
+
+const options = {}
+const stream = pickStream()
+
+function pickStream() {
+  if (process.env.NODE_ENV != "production") {
+    return pretty()
+  }
+}
 
 module.exports = {
-  logger: Pino(),
+  logger: Pino(stream),
 }
