5565ec474c866dd2e3d5e6ea0feadc4246120ecc
Peter Andrews <pmandrews@gmail.com>
2021-12-03 17:10:32 -0500

    Flesh out some of the quote command

--- commands/quote.js
+++ commands/quote.js
@@ -2,6 +2,9 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 
 const { determineName } = require("../services/speaker-name")
 const { gameForChannel } = require("../services/default-game-scope")
+const { Quotes, Lines, Speakers } = require("../models")
+const { makeQuote } = require("../services/quote-builder")
+const { logger } = require("../util/logger")
 
 module.exports = {
   name: "quote",
@@ -27,15 +30,27 @@ module.exports = {
       ),
   async execute(interaction) {
     const text = interaction.options.getString("text")
-    const speaker = interaction.options.getUser("speaker")
+    const speaker_user = interaction.options.getUser("speaker")
     const alias = interaction.options.getString("alias")
+    const user = interaction.user
 
-    const speaker_name = determineName(interaction.guild.members.fetch(speaker).nickname, speaker.username, alias)
-    const game = gameForChannel(interaction.channel)
+    const speaker_name = determineName(
+      interaction.guild.members.fetch(speaker_user).nickname,
+      speaker_user.username,
+      alias
+    )
+    const game = await gameForChannel(interaction.channel)
 
-    // get default game for current scope (channel, channel parent, possibly channel parent parent, guild)
-    //    offload to scope service
-    // if default game, save quote and reply
+    if (game) {
+      try {
+        await makeQuote(text, speaker_name, game, speaker_user)
+      } catch (error) {
+        logger.warn(error)
+        return interaction.reply("Something went wrong :-(")
+      }
+      return interaction.reply(`${user.name} quoted ${speaker_name}: ${text}`)
+    }
+
     // if no default game, store quote info and prompt for game
     //    that followup will need to retrieve the stored info, save for game, post a message, and ask if it should save the chosen game as the default for this channel
     //    the followup after that either saves the game as default or scraps it, replies either way
--- commands/quote.test.js
+++ commands/quote.test.js
@@ -1,13 +1,22 @@
 "use strict"
 
 const quote_command = require("./quote")
-const { Guilds, Games, DefaultGames } = require("../models")
+const {
+  Guilds,
+  Games,
+  DefaultGames,
+  Quotes,
+  Lines,
+  Speakers,
+} = require("../models")
+const DefaultGameScopeService = require("../services/default-game-scope")
 
 const { Interaction } = require("../testing/interaction")
 const { simpleflake } = require("simpleflakes")
 
 var guild
 var interaction
+var game
 
 beforeEach(async () => {
   try {
@@ -15,18 +24,33 @@ beforeEach(async () => {
       name: "Test Guild",
       snowflake: simpleflake().toString(),
     })
+    game = await Games.create({
+      name: "Test Game",
+      guildId: guild.id,
+    })
     interaction = new Interaction(guild.snowflake)
   } catch (err) {
     console.log(err)
   }
 
   interaction.command_options.text = "Text of the quote"
-  interaction.command_options.speaker = {}
+  interaction.command_options.speaker = {
+    username: "Test Speaker User",
+    id: simpleflake(),
+  }
   interaction.command_options.alias = "Dude Bro"
 })
 
 afterEach(async () => {
   try {
+    const games = await Games.findAll({ where: { guildId: guild.id } })
+    const game_ids = games.map((g) => g.id)
+    const quotes = await Quotes.findAll({ where: { gameId: game_ids } })
+    const quote_ids = quotes.map((q) => q.id)
+
+    await Lines.destroy({ where: { quoteId: quote_ids } })
+    await Quotes.destroy({ where: { gameId: game_ids } })
+    await DefaultGames.destroy({ where: { gameId: game_ids } })
     await Games.destroy({ where: { guildId: guild.id } })
     await guild.destroy()
   } catch (err) {
@@ -35,15 +59,47 @@ afterEach(async () => {
 })
 
 describe("with a default game", () => {
-  it.todo("saves the quote for the default game")
+  beforeEach(async () => {
+    await DefaultGames.create({
+      name: guild.name,
+      snowflake: guild.snowflake,
+      type: DefaultGames.TYPE_GUILD,
+      gameId: game.id,
+    })
+  })
+
+  it("saves the quote for the default game", async () => {
+    await quote_command.execute(interaction)
+    const quote = await Quotes.findOne({
+      where: { gameId: game.id },
+      include: Lines,
+    })
+
+    expect(quote.Lines[0].content).toEqual(interaction.command_options.text)
+  })
 
   describe("reply", () => {
-    it.todo("says who saved the quote")
+    beforeEach(() => {
+      jest
+        .spyOn(DefaultGameScopeService, "gameForChannel")
+        .mockImplementation((chan) => game)
+    })
 
-    it("displays the quote and speaker", async () => {
+    it("says who saved the quote", async () => {
+      const reply = await quote_command.execute(interaction)
+
+      expect(reply).toMatch(interaction.user.name)
+    })
+
+    it("displays the quote text", async () => {
       const reply = await quote_command.execute(interaction)
 
       expect(reply).toMatch(interaction.command_options.text)
+    })
+
+    it("displays the quote speaker", async () => {
+      const reply = await quote_command.execute(interaction)
+
       expect(reply).toMatch(interaction.command_options.alias)
     })
   })
