bb8902fb5c2317592fa4849994400217bfc00453
Peter Andrews <pmandrews@gmail.com>
2021-12-05 21:50:58 -0500

    Warn about command errors

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,6 +3,7 @@ const { logger } = require("../util/logger")
 module.exports = {
   name: "interactionCreate",
   execute(interaction) {
+    // handle command invocations
     if (interaction.isCommand() || interaction.isApplicationCommand()) {
       const command = interaction.client.commands.get(interaction.commandName)
 
@@ -11,7 +12,7 @@ module.exports = {
       try {
         command.execute(interaction)
       } catch (error) {
-        logger.debug(error)
+        logger.warn(error)
         interaction.reply({
           content: "There was an error while executing this command!",
           ephemeral: true,
@@ -19,6 +20,7 @@ module.exports = {
       }
     }
 
+    // handle choices on select menu components
     if (interaction.isSelectMenu()) {
       const followup = interaction.client.followups.get(interaction.customId)
 
@@ -27,7 +29,7 @@ module.exports = {
       try {
         followup.execute(interaction)
       } catch (error) {
-        logger.debug(error)
+        logger.warn(error)
         interaction.reply({
           content: "There was an error while executing this command!",
           ephemeral: true,
