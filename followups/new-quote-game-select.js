96210f110952fa4296ec2f0ed530af93bc1ac5d2
Peter Andrews <pmandrews@gmail.com>
2021-12-06 16:35:46 -0500

    Add game select followup for new quote

---
+++ followups/new-quote-game-select.js
@@ -0,0 +1,20 @@
+const { Quotes, Games } = require("../models")
+const { makeQuote } = require("../services/quote-builder")
+const { keyv } = require("../util/keyv.js")
+
+module.exports = {
+  name: "newQuoteGameSelect",
+  async execute(interaction) {
+    const game = await Games.findByPk(interaction.values[0])
+    interaction.update({ content: `Chose ${game.name}`, components: [] })
+
+    options = await keyv.get(interaction.message.interaction.id.toString())
+
+    const result = await makeQuote(options.text, options.attribution, game, options.speaker_user)
+
+    if(! (result instanceof Quotes)) {
+      return interaction.followUp("Something went wrong :-(")
+    }
+    return interaction.followUp(`${interaction.user.username} quoted ${options.attribution}: ${options.text}`)
+  },
+}
---
+++ followups/new-quote-game-select.test.js
@@ -0,0 +1,99 @@
+const Followup = require("./new-quote-game-select")
+
+const {
+  Guilds,
+  Games,
+  Quotes,
+  Lines,
+} = require("../models")
+const { Interaction } = require("../testing/interaction")
+const { simpleflake } = require("simpleflakes")
+const { keyv } = require("../util/keyv.js")
+const { QuoteData } = require("../services/quote-builder")
+
+var guild
+var interaction
+var game
+const quote_options = new QuoteData({
+  text: "Test quote text",
+  attribution: "The Person",
+  speaker_user: {
+    id: 1,
+    username: "TheFiddler"
+  }
+})
+const caller_id = simpleflake()
+
+beforeEach(async () => {
+  try {
+    guild = await Guilds.create({
+      name: "Test Guild",
+      snowflake: simpleflake().toString(),
+    })
+    game = await Games.create({
+      name: "Test Game",
+      guildId: guild.id,
+    })
+    interaction = new Interaction(guild.snowflake)
+  } catch (err) {
+    console.log(err)
+  }
+
+  interaction.values = [game.id.toString()]
+  interaction.message.interaction = {id: caller_id}
+  keyv.set(caller_id, quote_options)
+})
+
+afterEach(async () => {
+  try {
+    const games = await Games.findAll({ where: { guildId: guild.id } })
+    const game_ids = games.map((g) => g.id)
+    const quotes = await Quotes.findAll({ where: { gameId: game_ids } })
+    const quote_ids = quotes.map((q) => q.id)
+
+    await Lines.destroy({ where: { quoteId: quote_ids } })
+    await Quotes.destroy({ where: { gameId: game_ids } })
+    await Games.destroy({ where: { guildId: guild.id } })
+    await guild.destroy()
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+it("saves the quote to the chosen game", async () => {
+  await Followup.execute(interaction)
+  const quote = await Quotes.findOne({
+    where: { gameId: game.id },
+    include: Lines,
+  })
+
+  expect(quote.Lines[0].content).toEqual(quote_options.text)
+})
+
+describe("reply", () => {
+  it("says who saved the quote", async () => {
+    const reply = await Followup.execute(interaction)
+
+    expect(reply).toMatch(interaction.user.username)
+  })
+
+  it("displays the quote text", async () => {
+    const reply = await Followup.execute(interaction)
+
+    expect(reply).toMatch(quote_options.text)
+  })
+
+  it("displays the quote speaker", async () => {
+    const reply = await Followup.execute(interaction)
+
+    expect(reply).toMatch(quote_options.attribution)
+  })
+
+  it("notifies if there is an error", async () => {
+    jest.spyOn(Quotes, 'create').mockImplementation((...options) => {throw new Error("test error")})
+
+    const reply = await Followup.execute(interaction)
+
+    expect(reply).toMatch("Something went wrong")
+  })
+})
