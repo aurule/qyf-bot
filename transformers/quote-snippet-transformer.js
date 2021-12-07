55dab1d8d13192c349e8f3b8f487266113eb4748
Peter Andrews <pmandrews@gmail.com>
2021-12-07 16:13:17 -0500

    Add quote transformer

--- transformers/quote-snippet-transformer.js
+++ transformers/quote-snippet-transformer.js
@@ -1,16 +1,26 @@
 /**
  * Return a formatted snippet that displays the passed quotes' lines
  *
+ * The format renders like:
+ *
+ * > **attribution:** line 0 text
+ * > **att:* line 1 text
+ *
+ * > **att:** line 0 text quote 2
+ *
  * @param  {Array<Quotes>} quotes Array of quote objects with Lines included
  * @return {String}               Markdown-formatted string of the quotes' contents
  */
-exports.transform = (quotes) => {
-  // general formatting is:
-  // > **attribution:** line 0 text
-  // > **att:* line 1 text
-  //
-  // > **att:** line 0 text quote 2
-  //
-  // etc
-  return ""
+function transform(quotes) {
+  return quotes
+    .map((quote) => {
+      return quote.Lines.map((line) => {
+        return `> **${line.speakerAlias}:** ${line.content}`
+      }).join("\n")
+    })
+    .join("\n\n")
+}
+
+module.exports = {
+  transform,
 }
---
+++ transformers/quote-snippet-transformer.test.js
@@ -0,0 +1,155 @@
+const { transform } = require("./quote-snippet-transformer")
+
+const { Guilds, Games, Quotes, Lines } = require("../models")
+
+const { simpleflake } = require("simpleflakes")
+
+var guild
+var game
+
+beforeEach(async () => {
+  try {
+    guild = await Guilds.create({
+      name: "Test Guild",
+      snowflake: simpleflake().toString(),
+    })
+    game = await Games.create({
+      name: "Test Game",
+      guildId: guild.id,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+afterEach(async () => {
+  try {
+    const games = await Games.findAll({ where: { guildId: guild.id } })
+    const game_ids = games.map((g) => g.id)
+    const quotes = await Quotes.findAll({ where: { gameId: game_ids } })
+    const quote_ids = quotes.map((q) => q.id)
+
+    await Lines.destroy({ where: { quoteId: quote_ids } })
+    await Quotes.destroy({ where: { gameId: game_ids } })
+    await Games.destroy({ where: { guildId: guild.id } })
+    await guild.destroy()
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+it("shows the text of each line in a quote", async () => {
+  await Quotes.create(
+    {
+      saidAt: Date.now,
+      gameId: game.id,
+      Lines: [
+        {
+          content: "First line",
+          lineOrder: 0,
+        },
+        {
+          content: "Second line",
+          lineOrder: 1,
+        },
+      ],
+    },
+    {
+      include: Lines,
+    }
+  )
+
+  const quotes = await Quotes.findAll({
+    where: { gameId: game.id },
+    include: Lines,
+  })
+
+  const result = transform(quotes)
+
+  expect(result).toMatch("First line")
+  expect(result).toMatch("Second line")
+})
+
+it("shows the attribution of each line in a quote", async () => {
+  await Quotes.create(
+    {
+      saidAt: Date.now,
+      gameId: game.id,
+      Lines: [
+        {
+          content: "First line",
+          lineOrder: 0,
+          speakerAlias: "Person 1"
+        },
+        {
+          content: "Second line",
+          lineOrder: 1,
+          speakerAlias: "Person 2"
+        },
+      ],
+    },
+    {
+      include: Lines,
+    }
+  )
+
+  const quotes = await Quotes.findAll({
+    where: { gameId: game.id },
+    include: Lines,
+  })
+
+  const result = transform(quotes)
+
+  expect(result).toMatch("Person 1")
+  expect(result).toMatch("Person 2")
+})
+
+it("shows the text of all quotes", async () => {
+  await Quotes.create(
+    {
+      saidAt: Date.now,
+      gameId: game.id,
+      Lines: [
+        {
+          content: "First line",
+          lineOrder: 0,
+        },
+        {
+          content: "Second line",
+          lineOrder: 1,
+        },
+      ],
+    },
+    {
+      include: Lines,
+    }
+  )
+  await Quotes.create(
+    {
+      saidAt: Date.now,
+      gameId: game.id,
+      Lines: [
+        {
+          content: "First line quote 2",
+          lineOrder: 0,
+        },
+        {
+          content: "Second line quote 2",
+          lineOrder: 1,
+        },
+      ],
+    },
+    {
+      include: Lines,
+    }
+  )
+
+  const quotes = await Quotes.findAll({
+    where: { gameId: game.id },
+    include: Lines,
+  })
+
+  const result = transform(quotes)
+
+  expect(result).toMatch("quote 2")
+})
