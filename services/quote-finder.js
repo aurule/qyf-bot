8cbbb13c5974395b2b28946b6bc398156c1dc9dd
Peter Andrews <pmandrews@gmail.com>
2021-12-07 13:48:45 -0500

    Sketch out quote finder and transformer

---
+++ services/quote-finder.js
@@ -0,0 +1,53 @@
+const { Quotes, Lines } = require("../models")
+
+class Options {
+  constructor(options) {
+    // translate discord speaker to User object
+    this.speaker = options.speaker
+
+    // accept either a number or an array of numbers
+    this.gameId = options.gameId
+
+    /**
+     * Displayed attribution of at least one line
+     * @type {String}
+     */
+    this.alias = options.alias
+
+    /**
+     * Guild containing the quotes
+     * @type {Guild}
+     */
+    this.guild = options.guild
+
+    /**
+     * How many quotes to show
+     * @type {Number}
+     */
+    this.limit = options.limit
+  }
+}
+
+/**
+ * Find all quotes that match the given criteria
+ * @param  {Options}        options Search criteria and limit object
+ * @return {Array<Quotes>}          Array of Quote objects matching the criteria
+ */
+async function findAll(options) {
+  // build a query
+  //
+
+  // const game_ids = guild.Games.map((g) => g.id)
+  // const quotes = await Quotes.findAll({
+  //   where: { gameId: game_ids },
+  //   include: Lines,
+  //   limit: amount,
+  //   order: [['saidAt', 'DESC']],
+  // })
+  return Quotes.findAll()
+}
+
+module.exports = {
+  Options,
+  findAll,
+}
---
+++ transformers/quote-snippet-transformer.js
@@ -0,0 +1,16 @@
+/**
+ * Return a formatted snippet that displays the passed quotes' lines
+ *
+ * @param  {Array<Quotes>} quotes Array of quote objects with Lines included
+ * @return {String}               Markdown-formatted string of the quotes' contents
+ */
+exports.transform = (quotes) => {
+  // general formatting is:
+  // > **attribution:** line 0 text
+  // > **att:* line 1 text
+  //
+  // > **att:** line 0 text quote 2
+  //
+  // etc
+  return ""
+}
