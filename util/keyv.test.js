18325716a99af9349203c6727cea86cba462363b
Peter Andrews <pmandrews@gmail.com>
2021-12-07 15:22:08 -0500

    Add minimal tests for logger and keyv

---
+++ util/keyv.test.js
@@ -0,0 +1,19 @@
+var old_env
+
+beforeAll(() => {
+  old_env = process.env
+})
+
+beforeEach(() => {
+  jest.resetModules()
+  process.env = {...old_env}
+})
+
+afterAll(() => {
+  process.env = old_env
+})
+
+it("creates a keyv instance", () => {
+  const { keyv } = require("./keyv")
+  expect(keyv).toBeTruthy()
+})
---
+++ util/logger.test.js
@@ -0,0 +1,19 @@
+var old_env
+
+beforeAll(() => {
+  old_env = process.env
+})
+
+beforeEach(() => {
+  jest.resetModules()
+  process.env = {...old_env}
+})
+
+afterAll(() => {
+  process.env = old_env
+})
+
+it("creates a pino instance", () => {
+  const { logger } = require("./logger")
+  expect(logger).toBeTruthy()
+})
