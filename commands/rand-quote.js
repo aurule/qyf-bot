cc9dda70969af21e7690d6f3b689f5a93b202b00
Peter Andrews <pmandrews@gmail.com>
2021-12-10 16:12:39 -0500

    Implement random quote command

--- commands/rand-quote.js
+++ commands/rand-quote.js
@@ -1,4 +1,92 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+const { SlashCommandBuilder, userMention } = require("@discordjs/builders")
+
+const {
+  Guilds,
+  Games,
+  Quotes,
+  Lines,
+  DefaultGames,
+  Users,
+  sequelize,
+} = require("../models")
+const GameChoicesTransformer = require("../transformers/game-choices-transformer")
+const QuoteFinder = require("../services/quote-finder")
+const QuoteSnippetTransformer = require("../transformers/quote-snippet-transformer")
+const { gameForChannel } = require("../services/default-game-scope")
+
+/**
+ * Get the correct game or fall back on default data
+ *
+ * If game_arg is provided, it will look up that game and ignore the default.
+ * If it is not provided and there is a default, it will return the default.
+ * If it is not provided and there is no default, it will return a null object:
+ * {
+ *    id: null,
+ *    name: "all games",
+ *  }
+ *
+ * @param  {Int}      game_arg  ID of the game to use
+ * @param  {Channel}  channel   Discord channel object to use for finding a default
+ * @return {Game}               Game object or truncated lookalike with id and name properties
+ */
+async function getGameOrDefault(game_arg, channel) {
+  let null_game = {
+    id: null,
+    name: "all games",
+  }
+
+  var game
+  if (game_arg) {
+    game = await Games.findByPk(game_arg)
+  } else {
+    game = await gameForChannel(channel)
+  }
+
+  return game || null_game
+}
+
+/**
+ * Build a string that represents the filters and options to the command
+ * @param  {Int}              total           Total quotes found
+ * @param  {Games}            game            The game object the qutoes are from
+ * @param  {String}           quote_contents  The quotes themselves, already formatted
+ * @param  {String|null}      options.alias   The alias used to find quotes
+ * @param  {DiscordUser|null} options.speaker The speaker object used to find quotes
+ * @param  {String|null}      options.text    The text searched for in the quotes
+ * @return {String}                           The final human-readable output
+ */
+function describeResults(
+  total,
+  game,
+  quote_contents,
+  { alias, speaker, text } = {}
+) {
+  const desc_lines = []
+
+  if (total) {
+    desc_lines.push("Showing a random quote")
+  } else {
+    desc_lines.push("No quotes found")
+  }
+
+  desc_lines.push(`from ${game.name}`)
+
+  if (alias) {
+    desc_lines.push("by")
+    if (speaker) desc_lines.push(`${userMention(speaker.id)} as`)
+    desc_lines.push(`${alias}`)
+  } else if (speaker) desc_lines.push(`by ${userMention(speaker.id)}`)
+
+  if (text) desc_lines.push(`including "${text}"`)
+
+  const description = desc_lines.join(" ")
+
+  if (total) {
+    return `${description}:\n\n${quote_contents}`
+  } else {
+    return description
+  }
+}
 
 module.exports = {
   name: "rand-quote",
@@ -14,15 +102,65 @@ module.exports = {
     .addStringOption((option) =>
       option.setName("text").setDescription("Containing some text")
     )
-    .addStringOption((option) =>
-      option.setName("game").setDescription("From a game")
+    .addIntegerOption((option) =>
+      option
+        .setName("game")
+        .setDescription(
+          "Game the quote is from. Defaults to channel's current game"
+        )
+        .addChoices(GameChoicesTransformer.transform(guild.Games))
     ),
   async execute(interaction) {
     const speaker = interaction.options.getUser("speaker")
     const alias = interaction.options.getString("alias")
     const text = interaction.options.getString("text")
-    const game = interaction.options.getString("game")
+    const game_arg = interaction.options.getInteger("game")
 
-    await interaction.reply(`Showing a random quote`)
+    const guild = await Guilds.findByInteraction(interaction)
+    const game = await getGameOrDefault(game_arg, interaction.channel)
+
+    // This section is a workaround for a bug in sequelize that causes a SQL
+    // error we pass a snowflake to the quote finder. For a summary, see
+    // services/quote-finder.test.js near the bottom.
+    //
+    // To avoid that bug, we make a separate query for the user and manually
+    // handle the not-found case before passing the user ID to the finder.
+    let speaker_user = {}
+    if (speaker) {
+      const user = await Users.findOne({
+        where: { snowflake: speaker.id.toString() },
+      })
+      if (!user) {
+        return interaction.reply(
+          describeResults(0, game, "", {
+            alias: alias,
+            speaker: speaker,
+            text: text,
+          })
+        )
+      }
+      speaker_user = user
+    }
+
+    const finder_options = new QuoteFinder.SearchOptions({
+      userId: speaker_user.id,
+      alias: alias,
+      gameId: game.id,
+      text: text,
+      guild: guild,
+    })
+
+    const quote = await QuoteFinder.findOne(finder_options, { order: sequelize.random() })
+    const quote_contents = QuoteSnippetTransformer.transform(quote)
+
+    return interaction.reply(
+      describeResults(1, game, quote_contents, {
+        alias: alias,
+        speaker: speaker,
+        text: text,
+      })
+    )
   },
+  getGameOrDefault,
+  describeResults,
 }
---
+++ commands/rand-quote.test.js
@@ -0,0 +1,301 @@
+const rand_quotes_command = require("./rand-quote")
+
+const {
+  Guilds,
+  Games,
+  Quotes,
+  Lines,
+  DefaultGames,
+  Users,
+} = require("../models")
+const GameChoicesTransformer = require("../transformers/game-choices-transformer")
+const QuoteFinder = require("../services/quote-finder")
+
+const { Interaction } = require("../testing/interaction")
+const { simpleflake } = require("simpleflakes")
+
+var guild
+var interaction
+var game
+
+beforeEach(async () => {
+  try {
+    guild = await Guilds.create({
+      name: "Test Guild",
+      snowflake: simpleflake().toString(),
+    })
+    game = await Games.create({
+      name: "Test Game",
+      guildId: guild.id,
+    })
+    interaction = new Interaction(guild.snowflake)
+    interaction.command_options.speaker = undefined
+    interaction.command_options.alias = ""
+    interaction.command_options.text = ""
+    interaction.command_options.game = null
+    interaction.command_options.amount = null
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+afterEach(async () => {
+  try {
+    const games = await Games.findAll({ where: { guildId: guild.id } })
+    const game_ids = games.map((g) => g.id)
+    const quotes = await Quotes.findAll({ where: { gameId: game_ids } })
+    const quote_ids = quotes.map((q) => q.id)
+    const lines = await Lines.findAll({
+      where: { quoteId: quote_ids },
+      include: "speaker",
+    })
+    const line_speaker_ids = lines.map((line) => line.speaker.id)
+
+    await Lines.destroy({ where: { quoteId: quote_ids } })
+    await Quotes.destroy({ where: { gameId: game_ids } })
+    await Users.destroyByPk(line_speaker_ids)
+    await DefaultGames.destroy({ where: { gameId: game_ids } })
+    await Games.destroy({ where: { guildId: guild.id } })
+    await guild.destroy()
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+describe("execute", () => {
+  var speaker
+
+  beforeEach(async () => {
+    speaker = await Users.create({
+      name: "Speaker Man",
+      snowflake: simpleflake().toString(),
+    })
+  })
+
+  describe("game selection", () => {
+    it("sends the chosen game to the finder", async () => {
+      const finderSpy = jest.spyOn(QuoteFinder, "findOne")
+      interaction.command_options.game = game.id
+
+      await rand_quotes_command.execute(interaction)
+
+      // finderSpy was called with an object including gameId:game.id
+      expect(
+        finderSpy.mock.calls[finderSpy.mock.calls.length - 1][0]
+      ).toMatchObject({ gameId: [game.id] })
+    })
+  })
+
+  describe("quote contents", () => {
+    it("gets quote content from finder", async () => {
+      const finderSpy = jest.spyOn(QuoteFinder, "findOne")
+
+      await rand_quotes_command.execute(interaction)
+
+      expect(finderSpy).toHaveBeenCalled()
+    })
+
+    it("replies with the quote line text", async () => {
+      await Quotes.create(
+        {
+          saidAt: Date.now(),
+          gameId: game.id,
+          Lines: [
+            {
+              content: "Quote text is cool",
+              speakerId: speaker.id,
+              speakerAlias: "Some Dude",
+              lineOrder: 0,
+            },
+          ],
+        },
+        {
+          include: Lines,
+        }
+      )
+
+      const reply = await rand_quotes_command.execute(interaction)
+
+      expect(reply).toMatch("Quote text is cool")
+    })
+  })
+
+  describe("speaker", () => {
+    it("with an existing speaker, queries by speaker's user ID", async () => {
+      const finderSpy = jest.spyOn(QuoteFinder, "findOne")
+      interaction.command_options.speaker = {id: speaker.snowflake}
+
+      await rand_quotes_command.execute(interaction)
+
+      // finderSpy was called with an object including userId:speaker.id
+      expect(
+        finderSpy.mock.calls[finderSpy.mock.calls.length - 1][0]
+      ).toMatchObject({ userId: [speaker.id] })
+    })
+
+    it("with no existing speaker, it replies that there are no quotes", async () => {
+      interaction.command_options.speaker = {id: simpleflake().toString()}
+
+      const result = await rand_quotes_command.execute(interaction)
+
+      expect(result).toMatch("No quotes found")
+    })
+  })
+})
+
+describe("getGameOrDefault", () => {
+  describe("with a selected game", () => {
+    it("returns the chosen game", async () => {
+      const result = await rand_quotes_command.getGameOrDefault(
+        game.id,
+        interaction.channel
+      )
+
+      expect(result).toMatchObject({ id: game.id })
+    })
+  })
+
+  describe("with only a default game", () => {
+    beforeEach(async () => {
+      await DefaultGames.create({
+        gameId: game.id,
+        snowflake: guild.snowflake,
+        name: guild.name,
+        type: DefaultGames.TYPE_GUILD,
+      })
+    })
+
+    it("returns the default game", async () => {
+      const result = await rand_quotes_command.getGameOrDefault(
+        null,
+        interaction.channel
+      )
+
+      expect(result).toMatchObject({ id: game.id })
+    })
+  })
+
+  describe("without a default or chosen game", () => {
+    it("returns a null id", async () => {
+      const result = await rand_quotes_command.getGameOrDefault(
+        null,
+        interaction.channel
+      )
+
+      expect(result).toMatchObject({ id: null })
+    })
+
+    it("returns 'all games' text for game name", async () => {
+      const result = await rand_quotes_command.getGameOrDefault(
+        null,
+        interaction.channel
+      )
+
+      expect(result).toMatchObject({ name: "all games" })
+    })
+  })
+})
+
+describe("describeResults", () => {
+  describe("total quotes", () => {
+    it("with a total of one, it describes the single quote", () => {
+      const result = rand_quotes_command.describeResults(1, game, "things")
+
+      expect(result).toMatch("a random quote")
+    })
+
+    it("with a total of zero, it describes the lack of quotes", () => {
+      const result = rand_quotes_command.describeResults(0, game, "things")
+
+      expect(result).toMatch("No quotes found")
+    })
+
+    it("with a positive total, it shows the quote contents", () => {
+      const result = rand_quotes_command.describeResults(1, game, "things")
+
+      expect(result).toMatch("things")
+    })
+
+    it("with a zero total, it does not show the quote contents", () => {
+      const result = rand_quotes_command.describeResults(0, game, "things")
+
+      expect(result).not.toMatch("things")
+    })
+  })
+
+  it("shows the game name", () => {
+    const result = rand_quotes_command.describeResults(3, game, "things")
+
+    expect(result).toMatch("from Test Game")
+  })
+
+  it("includes the quote contents", () => {
+    const result = rand_quotes_command.describeResults(
+      3,
+      game,
+      "things that were said"
+    )
+
+    expect(result).toMatch("things that were said")
+  })
+
+  it("with an alias, says 'by alias'", () => {
+    const result = rand_quotes_command.describeResults(3, game, "things", {
+      alias: "alias",
+    })
+
+    expect(result).toMatch("by alias")
+  })
+
+  it("with an alias and a speaker, says 'by speaker as alias'", () => {
+    const speaker = { id: simpleflake().toString() }
+    const result = rand_quotes_command.describeResults(0, game, "things", {
+      speaker: speaker,
+      alias: "alias",
+    })
+
+    expect(result).toMatch(`by <@${speaker.id}> as alias`)
+  })
+
+  it("with a speaker, says 'by speaker'", () => {
+    const speaker = { id: simpleflake().toString() }
+    const result = rand_quotes_command.describeResults(0, game, "things", {
+      speaker: speaker,
+    })
+
+    expect(result).toMatch(`by <@${speaker.id}>`)
+  })
+
+  it("with search text, shows the text", () => {
+    const result = rand_quotes_command.describeResults(1, game, "things", {
+      text: "search text",
+    })
+
+    expect(result).toMatch('including "search text"')
+  })
+})
+
+describe("data", () => {
+  beforeEach(async () => {
+    await Games.bulkCreate([
+      { name: "Test Game 1", guildId: guild.id },
+      { name: "Test Game 2", guildId: guild.id },
+    ])
+    await guild.reload({ include: Games })
+  })
+
+  // This test is very bare-bones because we're really just
+  // testing that the various calls to discord.js functions
+  // were executed properly.
+  it("returns something", async () => {
+    const command_data = rand_quotes_command.data(guild)
+
+    expect(command_data).toBeTruthy()
+  })
+
+  it("uses the command's name", () => {
+    const command_data = rand_quotes_command.data(guild)
+
+    expect(command_data.name).toEqual(rand_quotes_command.name)
+  })
+})
