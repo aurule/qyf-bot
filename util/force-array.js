c3a98d1fc72197da2462997608284b10641aeacb
Peter Andrews <pmandrews@gmail.com>
2021-12-10 15:58:19 -0500

    Return empty array when receiving plain null value

--- util/force-array.js
+++ util/force-array.js
@@ -1,5 +1,6 @@
 module.exports = {
   forceArray(obj) {
+    if(obj === null) return []
     return Array.isArray(obj) ? obj : [obj]
   },
 }
--- util/force-array.test.js
+++ util/force-array.test.js
@@ -11,3 +11,9 @@ it("passes through an array", () => {
 
   expect(result).toEqual([1])
 })
+
+it("returns an empty array with null arg", () => {
+  const result = forceArray(null)
+
+  expect(result).toEqual([])
+})
