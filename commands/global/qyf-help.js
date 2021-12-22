c4bfeae31a46e189e162ef1d9d6737ec52f07ca0
Peter Andrews <pmandrews@gmail.com>
2021-12-22 13:55:16 -0500

    Fix help command formatting

--- commands/global/qyf-help.js
+++ commands/global/qyf-help.js
@@ -8,28 +8,33 @@ const { stripIndent } = require("common-tags")
 
 module.exports = {
   name: "qyf-help",
-  data: () =>
-    new SlashCommandBuilder()
+  data() {
+    return new SlashCommandBuilder()
       .setName("qyf-help")
       .setDescription("Get help with qyf-bot and its commands")
-      .addStringOption(
-        (option) =>
-          option.setName("topic").setDescription("The topic you want help with")
-        .addChoices(Topics.map(t => [`${t.title}`, `${t.name}`]))
+      .addStringOption((option) =>
+        option
+          .setName("topic")
+          .setDescription("The topic you want help with")
+          .addChoices(Topics.map((t) => [`${t.title}`, `${t.name}`]))
       )
       .addStringOption((option) =>
         option
           .setName("command")
           .setDescription("The command you want help with")
           .addChoices(CommandChoicesTransformer.transform(commandFetch.all()))
-      ),
+      )
+  },
   async execute(interaction) {
     const command_name_arg = interaction.options.getString("command")
     const topic_name = interaction.options.getString("topic")
 
     if (topic_name) {
       const topic = Topics.get(topic_name)
-      if (! topic) return interaction.reply(`No help is available for the topic "${topic_name}"`)
+      if (!topic)
+        return interaction.reply(
+          `No help is available for the topic "${topic_name}"`
+        )
 
       return interaction.reply(topic.help())
     }
@@ -37,12 +42,16 @@ module.exports = {
     const command_name = command_name_arg || module.exports.name
     const command = interaction.client.commands.get(command_name)
 
-    if (! command?.help) return interaction.reply(`No help is available for the command "${command_name}"`)
+    if (!command?.help)
+      return interaction.reply(
+        `No help is available for the command "${command_name}"`
+      )
 
     // return reply with the command's help text
     return interaction.reply(CommandHelpPresenter.present(command))
   },
-  help: ({ command_name }) => stripIndent`
+  help({ command_name }) {
+    return stripIndent`
     ${command_name} shows helpful information about a command or topic.
 
     Args:
@@ -52,5 +61,6 @@ module.exports = {
     Both args let you pick from a list, so you don't need to memorize command or topic names.
 
     If you give both a command and a topic, ${command_name} will only show help for the topic.
-  `,
+  `
+  },
 }
