2074449ef8f81c99fc4023d244119301c03676ea
Peter Andrews <pmandrews@gmail.com>
2021-12-22 15:14:52 -0500

    Clean up permissions help formatting

--- help/permissions.js
+++ help/permissions.js
@@ -1,17 +1,24 @@
-const { stripIndent } = require("common-tags")
-const { inlineCode } = require('@discordjs/builders');
+const { inlineCode } = require("@discordjs/builders")
+const { stripIndent, oneLine } = require("common-tags")
 
 module.exports = {
   name: "permissions",
   title: "Command Permissions",
-  help: () => stripIndent`
-    Most of qyf-bot's commands can be used by anyone in the server who has the "Use slash commands" privilege.
-
-    There are some commands which are restricted to guild managers and channel managers. Anyone else who tries
-    to use one of these commands will see an error message. The privileged commands are:
-    • ${inlineCode("/add-game")}
-    • ${inlineCode("/update-game")}
-    • ${inlineCode("/set-default-game")}
-    • ${inlineCode("/remove-default-game")}
-  `,
+  help() {
+    return [
+      "Most of qyf-bot's commands can be used by anyone in the server who has the *Use Application Commands* permission.",
+      "",
+      oneLine`
+        There are some commands which are restricted to users with the *Manage Channels* or *Manage Server*
+        permissions. Anyone else who tries to use one of these commands will see an error message. The
+        privileged commands are:
+      `,
+      stripIndent`
+        • ${inlineCode("/add-game")}
+        • ${inlineCode("/update-game")}
+        • ${inlineCode("/set-default-game")}
+        • ${inlineCode("/remove-default-game")}
+      `,
+    ].join("\n")
+  },
 }
