00cc36095da707dd996e7a6804947375e354905d
Peter Andrews <pmandrews@gmail.com>
2021-12-23 19:42:33 -0500

    Add new completer for games with All Games option

---
+++ completers/game-name-with-all-completer.js
@@ -0,0 +1,32 @@
+const { Guilds, Games } = require("../models")
+const GamesForGuild = require("../caches/games-for-guild")
+
+/**
+ * The special value for the all games selector
+ * @type {Number}
+ */
+const ALL_GAMES = -1
+
+module.exports = {
+  /**
+   * Create and send autocomplete data for game name
+   *
+   * The value returned is the game ID in string format
+   *
+   * @param  {AutocompleteInteraction} interaction  Discord autocomplete interaction object
+   * @return {Promise}                              A promise for the interaction response
+   */
+  async complete(interaction) {
+    const data = await GamesForGuild.get(interaction.guildId.toString())
+
+    const options = data
+      .filter((game) => game.name.toLowerCase().includes(interaction.options.getFocused()))
+      .map((game) => {
+        return { name: game.name, value: game.id.toString() }
+      })
+    options.push({ name: "All Games", value: ALL_GAMES})
+
+    return interaction.respond(options)
+  },
+  ALL_GAMES,
+}
---
+++ completers/game-name-with-all-completer.test.js
@@ -0,0 +1,72 @@
+const GameNameWithAllCompleter = require("./game-name-with-all-completer")
+const { Guilds, Games } = require("../models")
+
+const { Interaction } = require("../testing/interaction")
+const { simpleflake } = require("simpleflakes")
+const { cache } = require("../util/keyv")
+
+var interaction
+var guild
+var game
+
+describe("complete", () => {
+  beforeEach(async () => {
+    try {
+      guild = await Guilds.create({
+        name: "Test Guild",
+        snowflake: simpleflake().toString(),
+      })
+
+      game = await Games.create({
+        guildId: guild.id,
+        name: "Test Game Partialities"
+      })
+      interaction = new Interaction(guild.snowflake)
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  afterEach(async () => {
+    try {
+      await game.destroy()
+      await guild.destroy()
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  it("gets games that match the guild", async () => {
+    const wrong_guild = await Guilds.create({
+      name: "Wrong Guild",
+      snowflake: simpleflake().toString(),
+    })
+    const wrong_game = await Games.create({
+      name: "Wrong Game Partial",
+      guildId: wrong_guild.id,
+    })
+
+    const result = await GameNameWithAllCompleter.complete(interaction)
+
+    expect(result.length).toEqual(2)
+    expect(result[0]).not.toMatchObject({ name: wrong_game.name })
+    expect(result[0]).toMatchObject({ name: game.name })
+
+    wrong_game.destroy()
+    wrong_guild.destroy()
+  })
+
+  it("gets games that match the partial text", async () => {
+    const result = await GameNameWithAllCompleter.complete(interaction)
+
+    expect(result[0]).toMatchObject({ name: game.name, value: `${game.id}` })
+  })
+
+  it("gracefully handles no results", async () => {
+    interaction.partial_text = "nothing matches"
+
+    const result = await GameNameWithAllCompleter.complete(interaction)
+
+    expect(result.length).toEqual(1)
+  })
+})
