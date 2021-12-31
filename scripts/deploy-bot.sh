fabb66d354af39f0a15748d209165394e18235d1
Peter Andrews <pmandrews@gmail.com>
2021-12-31 18:59:17 -0500

    Simplify deployment script

--- scripts/deploy-bot.sh
+++ scripts/deploy-bot.sh
@@ -2,12 +2,9 @@
 
 # this script is meant to be run from the bot's main directory
 
-. ~/.nvm/nvm.sh
 . ~/.profile
 . ~/.bashrc
 
-nvm exec default pm2 stop qyf-bot
-nvm install
 npm install
 npm run migrate
-nvm exec default pm2 start index.js --name qyf-bot
+pm2 reload qyf-bot
