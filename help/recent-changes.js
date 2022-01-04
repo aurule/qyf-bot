48e684479c1ded0721c2bbb5da4ea8e77a20a833
Peter Andrews <pmandrews@gmail.com>
2022-01-03 20:03:34 -0500

    Add changelog help topic

---
+++ help/recent-changes.js
@@ -0,0 +1,27 @@
+const { hideLinkEmbed } = require("@discordjs/builders")
+const { stripIndent, oneLine } = require("common-tags")
+const fs = require("fs")
+const path = require("path")
+
+const { version } = require("../package.json")
+
+function getChangelog() {
+  try {
+    return fs.readFileSync(path.join(__dirname, "../changelog", `${version}.md`))
+  } catch(error) {
+    return `no changelog for ${version}`
+  }
+}
+
+module.exports = {
+  name: "changes",
+  title: "Recent Changes",
+  help() {
+    return [
+      `qyf-bot is on version ${version}. Here's what's new!`,
+      "",
+      getChangelog(),
+      `Older change logs can be found on github: ${hideLinkEmbed("https://github.com/aurule/qyf-bot/tree/main/changelog")}`,
+    ].join("\n")
+  },
+}
