6eb3395c78bea05083ba4f2199b95c555fef63fa
Peter Andrews <pmandrews@gmail.com>
2022-01-07 14:27:27 -0500

    Add helper to create changelog snippet files

---
+++ scripts/add-changelog-snippet.js
@@ -0,0 +1,37 @@
+const fs = require("fs")
+const path = require("path")
+
+const prompts = require("prompts");
+
+(async () => {
+  const questions = [
+    {
+      type: "select",
+      name: "type",
+      message: "Pick the type of change",
+      choices: [
+        { title: "Addition", value: "added" },
+        { title: "Change", value: "changed" },
+        { title: "Removal", value: "removed" },
+        { title: "Fix", value: "fixed" },
+      ],
+    },
+    {
+      type: "text",
+      name: "message",
+      message: "One-line summary of the change",
+    },
+    {
+      type: "text",
+      name: "label",
+      message: "Shorthand reference to jog your memory",
+    },
+  ]
+
+  const response = await prompts(questions)
+
+  fs.writeFileSync(
+    path.join(__dirname, "../changes", `${response.label}.${response.type}`),
+    response.message
+  )
+})()
