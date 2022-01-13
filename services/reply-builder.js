8aef6f3f0d00c3fa8c5eaa1c77da168aa2adb50d
Peter Andrews <pmandrews@gmail.com>
2022-01-13 16:01:56 -0500

    Document reply builder

--- services/reply-builder.js
+++ services/reply-builder.js
@@ -3,7 +3,19 @@
 const { memberNicknameMention } = require("@discordjs/builders")
 
 module.exports = {
-
+  /**
+   * Construct reply text for a quote action
+   *
+   * The reply takes the form of "@reporter quoted @speaker as alias: text"
+   *
+   * @param  {Discord user} options.reporter Discord user object for the line reporter
+   * @param  {Discord user} options.speaker  Discord user object for the line reporter
+   * @param  {string}       options.text     Text of the line
+   * @param  {string|null}  options.alias    Alias of the speaker. Optional.
+   * @param  {String}       options.action   Action the reporter took. Defaults to "quoted".
+   *
+   * @return {string}       The complete reply string
+   */
   quoteReply: ({reporter, speaker, text, alias = null, action = "quoted"}) => {
     const lines = []
 
