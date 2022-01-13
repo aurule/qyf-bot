d040b3da6f45fe174ed0711f94e12563e767625b
Peter Andrews <pmandrews@gmail.com>
2022-01-13 16:46:54 -0500

    Improve reply builder test organization

--- services/reply-builder.test.js
+++ services/reply-builder.test.js
@@ -18,46 +18,6 @@ describe("quoteReply", () => {
     expect(reply).toMatch(reporter.id.toString())
   })
 
-  it("includes the speaker id", () => {
-    const speaker = { id: simpleflake() }
-    const reporter = { id: simpleflake() }
-
-    const reply = replyBuilder.quoteReply({
-      speaker: speaker,
-      reporter: reporter,
-      text: "test text",
-    })
-
-    expect(reply).toMatch(speaker.id.toString())
-  })
-
-  it("includes the alias if given", () => {
-    const speaker = { id: simpleflake() }
-    const reporter = { id: simpleflake() }
-
-    const reply = replyBuilder.quoteReply({
-      speaker: speaker,
-      reporter: reporter,
-      alias: "a person",
-      text: "test text",
-    })
-
-    expect(reply).toMatch("as a person")
-  })
-
-  it("omits the alias reference if not given", () => {
-    const speaker = { id: simpleflake() }
-    const reporter = { id: simpleflake() }
-
-    const reply = replyBuilder.quoteReply({
-      speaker: speaker,
-      reporter: reporter,
-      text: "test text",
-    })
-
-    expect(reply).not.toMatch(" as ")
-  })
-
   it("includes the quote text", () => {
     const speaker = { id: simpleflake() }
     const reporter = { id: simpleflake() }
@@ -71,30 +31,74 @@ describe("quoteReply", () => {
     expect(reply).toMatch("test text")
   })
 
-  it("uses the quoted action by default", () => {
-    const speaker = { id: simpleflake() }
-    const reporter = { id: simpleflake() }
+  describe("with a normal speaker", () => {
+    it("includes the speaker id", () => {
+      const speaker = { id: simpleflake() }
+      const reporter = { id: simpleflake() }
 
-    const reply = replyBuilder.quoteReply({
-      speaker: speaker,
-      reporter: reporter,
-      text: "test text",
+      const reply = replyBuilder.quoteReply({
+        speaker: speaker,
+        reporter: reporter,
+        text: "test text",
+      })
+
+      expect(reply).toMatch(speaker.id.toString())
     })
 
-    expect(reply).toMatch("quoted")
+    it("includes the alias if given", () => {
+      const speaker = { id: simpleflake() }
+      const reporter = { id: simpleflake() }
+
+      const reply = replyBuilder.quoteReply({
+        speaker: speaker,
+        reporter: reporter,
+        alias: "a person",
+        text: "test text",
+      })
+
+      expect(reply).toMatch("as a person")
+    })
+
+    it("omits the alias reference if not given", () => {
+      const speaker = { id: simpleflake() }
+      const reporter = { id: simpleflake() }
+
+      const reply = replyBuilder.quoteReply({
+        speaker: speaker,
+        reporter: reporter,
+        text: "test text",
+      })
+
+      expect(reply).not.toMatch(" as ")
+    })
   })
 
-  it("uses custom action if given", () => {
-    const speaker = { id: simpleflake() }
-    const reporter = { id: simpleflake() }
+  describe("actions", () => {
+    it("uses the quoted action by default", () => {
+      const speaker = { id: simpleflake() }
+      const reporter = { id: simpleflake() }
 
-    const reply = replyBuilder.quoteReply({
-      speaker: speaker,
-      reporter: reporter,
-      text: "test text",
-      action: "added text from"
+      const reply = replyBuilder.quoteReply({
+        speaker: speaker,
+        reporter: reporter,
+        text: "test text",
+      })
+
+      expect(reply).toMatch("quoted")
     })
 
-    expect(reply).toMatch("added text from")
+    it("uses custom action if given", () => {
+      const speaker = { id: simpleflake() }
+      const reporter = { id: simpleflake() }
+
+      const reply = replyBuilder.quoteReply({
+        speaker: speaker,
+        reporter: reporter,
+        text: "test text",
+        action: "added text from"
+      })
+
+      expect(reply).toMatch("added text from")
+    })
   })
 })
