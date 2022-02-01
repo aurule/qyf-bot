8d48068842a1b17f6391db32650261ee5add1d07
Peter Andrews <pmandrews@gmail.com>
2022-02-01 12:03:38 -0500

    Use game name in list quotes command

--- commands/list-quotes.test.js
+++ commands/list-quotes.test.js
@@ -72,9 +72,24 @@ describe("execute", () => {
   })
 
   describe("game selection", () => {
+    it("works with a game from the completer", async () => {
+      interaction.partial_text = game.name
+      const completer = list_quotes_command.autocomplete.get("game")
+      const game_arg = await completer.complete(interaction).then((values) => values[0].value)
+      interaction.command_options.game = game_arg
+      const finderSpy = jest.spyOn(QuoteFinder, "findAll")
+
+      await list_quotes_command.execute(interaction)
+
+      // finderSpy was called with an object including gameId:game.id
+      expect(
+        finderSpy.mock.calls[finderSpy.mock.calls.length - 1][0]
+      ).toMatchObject({ gameId: [game.id] })
+    })
+
     it("sends the chosen game to the finder", async () => {
       const finderSpy = jest.spyOn(QuoteFinder, "findAll")
-      interaction.command_options.game = game.id
+      interaction.command_options.game = game.name
 
       await list_quotes_command.execute(interaction)
 
@@ -184,8 +199,9 @@ describe("getGameOrDefault", () => {
   describe("with a selected game", () => {
     it("returns the chosen game", async () => {
       const result = await list_quotes_command.getGameOrDefault(
-        game.id,
-        interaction.channel
+        game.name,
+        interaction.channel,
+        guild.id,
       )
 
       expect(result).toMatchObject({ id: game.id })
@@ -205,7 +221,8 @@ describe("getGameOrDefault", () => {
     it("returns the default game", async () => {
       const result = await list_quotes_command.getGameOrDefault(
         null,
-        interaction.channel
+        interaction.channel,
+        guild.id,
       )
 
       expect(result).toMatchObject({ id: game.id })
@@ -216,7 +233,8 @@ describe("getGameOrDefault", () => {
     it("returns a null id", async () => {
       const result = await list_quotes_command.getGameOrDefault(
         null,
-        interaction.channel
+        interaction.channel,
+        guild.id,
       )
 
       expect(result).toMatchObject({ id: null })
@@ -225,7 +243,8 @@ describe("getGameOrDefault", () => {
     it("returns 'all games' text for game name", async () => {
       const result = await list_quotes_command.getGameOrDefault(
         null,
-        interaction.channel
+        interaction.channel,
+        guild.id,
       )
 
       expect(result).toMatchObject({ name: "all games" })
@@ -235,8 +254,9 @@ describe("getGameOrDefault", () => {
   describe("with the special All Game", () => {
     it("returns a null id", async () => {
       const result = await list_quotes_command.getGameOrDefault(
-        -1,
-        interaction.channel
+        "All Games",
+        interaction.channel,
+        guild.id,
       )
 
       expect(result).toMatchObject({ name: "all games" })
