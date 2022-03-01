f1b4446b9a7195ad249aa6a716aa5a8c56efed61
Peter Andrews <pmandrews@gmail.com>
2022-03-01 11:50:08 -0500

    Add hash arg to deploy script

--- scripts/deploy-global-commands.js
+++ scripts/deploy-global-commands.js
@@ -1,5 +1,14 @@
+const parseArgs = require("minimist")
+
 const commandService = require("../services/command-deploy");
 
+/**
+ * Deploy the global commands to Discord
+ *
+ * Args:
+ *   --hash HASH  The hash of existing command JSON. Optional.
+ */
 (async () => {
-  await commandService.deployGlobals()
+  const argv = parseArgs(process.argv.slice(2))
+  await commandService.deployGlobals(argv.hash)
 })()
