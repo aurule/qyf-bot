6f7e194d3d2e38b911faf4cb2dbf8764b8f65854
Peter Andrews <pmandrews@gmail.com>
2022-03-01 11:30:46 -0500

    Add command hash function and hash bypass on global deploys

--- services/command-deploy.js
+++ services/command-deploy.js
@@ -29,10 +29,45 @@ function buildGlobalCommandJSON() {
 }
 
 /**
- * Push the global command JSON
- * @return {Promise}      Promise for the http call
+ * Generate a unique hash for the global command JSON structure
+ * @return {string} Hash of the JSON data about global slash commands
  */
-async function deployGlobals() {
+function hashGlobalCommandJSON() {
+  // Quick hash function courtesy of https://stackoverflow.com/users/815680/bryc found
+  // at https://stackoverflow.com/a/52171480
+  const cyrb53 = function(str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+    return (h2>>>0).toString(16).padStart(8,0)+(h1>>>0).toString(16).padStart(8,0);
+  };
+
+  // use fn from module.exports so we can mock it out in tests
+  return cyrb53(JSON.stringify(module.exports.buildGlobalCommandJSON()))
+}
+
+/**
+ * Push the global command JSON
+ *
+ * If given a hash, no deploy will happen if that hash matches the hash of our current command json.
+ *
+ * @param  {string}   hash  Optional hash of old command json
+ * @return {Promise}        Promise for the http call
+ */
+async function deployGlobals(hash = null) {
+  if(hash) {
+    const newHash = module.exports.hashGlobalCommandJSON()
+    if (hash === newHash) {
+      logger.info("Global commands have not changed, skipping deploy")
+      return
+    }
+  }
+
   logger.info("Deploying global commands")
 
   return restClient()
@@ -77,6 +112,7 @@ async function deployDev() {
 
 module.exports = {
   buildGlobalCommandJSON,
+  hashGlobalCommandJSON,
   restClient,
   deployGlobals,
   deployDev,
--- services/command-deploy.test.js
+++ services/command-deploy.test.js
@@ -11,6 +11,24 @@ describe("buildGlobalCommandJSON", () => {
   })
 })
 
+describe("hashGlobalCommandJSON", () => {
+  it("returns a string hash", () => {
+    const result = commandService.hashGlobalCommandJSON()
+
+    expect(typeof result).toEqual("string")
+  })
+
+  it("gives a different hash with different json", () => {
+    jest.spyOn(commandService, "buildGlobalCommandJSON")
+      .mockReturnValueOnce("abcdefg")
+      .mockReturnValueOnce("bbcdefg")
+    const first_result = commandService.hashGlobalCommandJSON()
+    const second_result = commandService.hashGlobalCommandJSON()
+
+    expect(first_result).not.toEqual(second_result)
+  })
+})
+
 describe("deployGlobals", () => {
   beforeEach(() => {
     jest
@@ -24,5 +42,14 @@ describe("deployGlobals", () => {
     const result = await commandService.deployGlobals()
 
     expect(routeSpy).toHaveBeenCalled()
+  })
+
+  it("with a matching hash, skips deploy", async () => {
+    jest.spyOn(commandService, "hashGlobalCommandJSON").mockReturnValue("testhash")
+    const routeSpy = jest.spyOn(Routes, "applicationCommands").mockReturnValue("/")
+
+    const result = await commandService.deployGlobals("testhash")
+
+    expect(routeSpy).not.toHaveBeenCalled()
   })
 })
