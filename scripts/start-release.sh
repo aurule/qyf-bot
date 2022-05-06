7ec625c1bb44d66d9ed89e3fc636060ebb32fbf0
Peter Andrews <pmandrews@gmail.com>
2022-05-06 14:27:20 -0400

    Automatically commit new version in package files

--- scripts/start-release.sh
+++ scripts/start-release.sh
@@ -2,10 +2,8 @@
 
 # this script is meant to be run from the bot's main directory
 
-# . ~/.profile
-# . ~/.bashrc
-
 git branch release/$1
 git checkout release/$1
 npm --no-git-tag-version version $1
 npm run changelog:build
+git commit package.json package-lock.json -m "Update version to $1"
